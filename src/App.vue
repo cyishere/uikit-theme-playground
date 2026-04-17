de3801ad633afdfbd6d2ff8222b2820e36625b40
Chen Yang <cyishere@gmail.com>
2026-04-17 14:23:40 +0800

    Setup the project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import PreviewCanvas from './components/PreviewCanvas.vue';
+import Sidebar from './components/SideBar.vue';
+import Toolbar from './components/ToolBar.vue';
+</script>
+
+<template>
+  <Sidebar />
+  <main>
+    <Toolbar />
+    <PreviewCanvas />
+  </main>
+</template>
+
+<style scoped></style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="tp-sidebar">
+    <h1>Sidebar</h1>
+  </div>
+</template>
+
+<style scoped>
+.tp-sidebar {
+  display: none;
+
+  @media (min-width: 769px) {
+    display: block;
+  }
+}
+</style>
